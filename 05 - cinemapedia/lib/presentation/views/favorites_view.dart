803c0659b71915c31277ae49b65fe34448eb36ca
Joaquin Gonzalez <joaquingonzalez008@gmail.com>
2024-05-17 18:04:28 -0300

    favorites

--- 05 - cinemapedia/lib/presentation/views/favorites_view.dart
+++ 05 - cinemapedia/lib/presentation/views/favorites_view.dart
@@ -1,15 +1,38 @@
+import 'package:cinemapedia/providers/storage/favorite_movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMovieProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(favoriteMovieProvider).entries.map((movie) => movie.value).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favorites View'),
       ),
-      body: const Center(child: Text('Favoritos')),
+      body: ListView.builder(
+        itemCount: favoritesMovies.length,
+        itemBuilder: (BuildContext context, int index) {
+          final movie = favoritesMovies[index];
+          return ListTile(
+            title: Text(movie.title),
+          );
+        },
+      ),
     );
   }
 }
---
+++ 05 - cinemapedia/lib/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,33 @@
+import 'package:cinemapedia/domain/movie/infrastructure/local/abstracts/service/a_local_storage_service.dart';
+import 'package:cinemapedia/domain/movie/model/movie.dart';
+import 'package:cinemapedia/providers/storage/local_storage_service_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoriteMovieProvider = StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+  final service = ref.watch(localStorageServiceProvider);
+  return StorageMoviesNotifier(service: service);
+});
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final ALocalStorageServie service;
+
+  StorageMoviesNotifier({
+    required this.service,
+  }) : super({});
+
+  Future<List<Movie>> loadNextPage() async {
+    final movies = await service.loadMovies(offset: page * 20);
+    page++;
+
+    final Map<int, Movie> dic = {};
+
+    for (final movie in movies) {
+      dic[movie.id] = movie;
+    }
+
+    state = {...state, ...dic};
+
+    return movies;
+  }
+}
