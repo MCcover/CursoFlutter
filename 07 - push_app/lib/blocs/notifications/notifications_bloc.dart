05b0a24ef07d3a496f21f39506c23cc76a658c18
Joaquin Gonzalez <joaquingonzalez008@gmail.com>
2024-10-15 09:17:41 -0300

    messaging background

--- 07 - push_app/lib/blocs/notifications/notifications_bloc.dart
+++ 07 - push_app/lib/blocs/notifications/notifications_bloc.dart
@@ -1,16 +1,76 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/domain/push_message.dart';
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState()) {
-    // on<NotificationsEvent>((event, emit) {
-    // });
+    on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    _initialStatusCheck();
+    _onForegroundMessage();
+  }
+
+  static Future<void> initializeFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _notificationStatusChanged(NotificationStatusChanged event, Emitter<NotificationsState> emit) {
+    emit(
+      state.copyWith(status: event.status),
+    );
+    _getFCMToken();
+  }
+
+  void _getFCMToken() async {
+    final settings = await messaging.getNotificationSettings();
+    if (settings.authorizationStatus != AuthorizationStatus.authorized) {
+      return;
+    }
+
+    final fcmToken = await messaging.getToken();
+    // ignore: avoid_print
+    print("FCMToken $fcmToken");
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) {
+    if (message.notification == null) {
+      return;
+    }
+
+    final notification = PushMessage(
+      messageId: message.messageId?.replaceAll(':', '').replaceAll('%', '') ?? '',
+      title: message.notification!.title ?? '',
+      body: message.notification!.body ?? '',
+      sendDate: message.sentTime ?? DateTime.now(),
+      data: message.data,
+      imageUrl: Platform.isAndroid ? message.notification!.android?.imageUrl : message.notification!.apple?.imageUrl,
+    );
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    // await messaging.setAutoInitEnabled(true);
+    add(NotificationStatusChanged(settings.authorizationStatus));
   }
 
   void requestPermission() async {
@@ -24,12 +84,6 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       sound: true,
     );
 
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      // print('User granted permission');
-    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
-      // print('User granted provisional permission');
-    } else {
-      // print('User declined or has not accepted permission');
-    }
+    add(NotificationStatusChanged(settings.authorizationStatus));
   }
 }
--- 07 - push_app/lib/main.dart
+++ 07 - push_app/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +6,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:push_app/blocs/notifications/notifications_bloc.dart';
 import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
-import 'package:push_app/firebase_options.dart';
 
 import 'helpers/scrollBehavior/scroll_behavior.dart';
 import 'helpers/windowsSize/window_size.dart';
@@ -19,13 +17,11 @@ Future<void> main() async {
     setWindowSize(width: 400, height: 700);
   }
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  await FirebaseMessaging.instance.setAutoInitEnabled(true);
-  // ignore: avoid_print
-  print("FCMToken $fcmToken");
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  await NotificationsBloc.initializeFirebase();
 
   runApp(MultiBlocProvider(
     providers: [
